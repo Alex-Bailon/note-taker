d0d0a6299e846cc7206e4291c45b747e67236610
Alex B <abailon949@gmail.com>
2020-04-14 11:00:33 -0700

    added app post

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
-var app = express();
-var PORT = 3000;
+const app = express();
+let PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
@@ -15,8 +17,30 @@ app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./db/db.json"))
+});  
 
+app.post("/api/notes", function(req, res) {
+    var newNote = req.body;
+    fs.readFile('./db/db.json', 'utf-8', function(error, data) {
+        if (error) {
+          return console.log(error);
+        }
+        let dataRetun = JSON.parse(data)
+        dataRetun.push(newNote)
+        fs.writeFile('./db/db.json', JSON.stringify(dataRetun), function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+          });
+      });
+      
+});
 
+app.delete("/api/notes/:id",function(req, res) {
+    var chosen = req.params.id;
+
+})
 
 
 app.listen(PORT, function() {
