eb2a41bee3fef29cfca67da04188df31583bc24e
Alex B <abailon949@gmail.com>
2020-04-14 12:06:25 -0700

    added process.env.PORT to port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-let PORT = 3000;
+let PORT = PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
