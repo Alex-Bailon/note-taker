45d171ae90b95ee4ec4ad6e7762d788ee81148b5
Alex B <abailon949@gmail.com>
2020-04-15 15:56:59 -0700

    changed path from '/' to '*'

--- server.js
+++ server.js
@@ -9,10 +9,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'))
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
@@ -58,6 +54,10 @@ app.delete("/api/notes/:id",function(req, res) {
           });
       });
 })
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
